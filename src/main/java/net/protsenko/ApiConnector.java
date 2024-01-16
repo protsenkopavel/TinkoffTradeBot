0b81c6e618acf01627d434954263edf677155c51
Pavel Protsenko <pavelprotsenko@outlook.com>
2024-01-17 02:07:30 +0300

    First initilization

---
+++ src/main/java/net/protsenko/ApiConnector.java
@@ -0,0 +1,35 @@
+package net.protsenko;
+
+import lombok.extern.slf4j.Slf4j;
+import ru.tinkoff.invest.openapi.OpenApi;
+import ru.tinkoff.invest.openapi.model.rest.SandboxRegisterRequest;
+import ru.tinkoff.invest.openapi.okhttp.OkHttpOpenApi;
+
+@Slf4j
+public class ApiConnector {
+    private final Parameters parameters;
+    private OpenApi openApi;
+
+    public ApiConnector(Parameters parameters) {
+        this.parameters = parameters;
+    }
+
+    public OpenApi getOpenApi() throws Exception {
+        if (openApi == null) {
+            close();
+            log.info("Create Tinkoff API connection");
+            openApi = new OkHttpOpenApi(parameters.getToken(), parameters.isSandBoxMode());
+            if (openApi.isSandboxMode()) {
+                openApi.getSandboxContext().performRegistration(new SandboxRegisterRequest()).join();
+            }
+        }
+        return openApi;
+    }
+
+    public void close() throws Exception {
+        if (openApi != null) {
+            openApi.close();
+            log.info("Tinkoff API connection has been closed");
+        }
+    }
+}
---
+++ src/main/java/net/protsenko/Parameters.java
@@ -0,0 +1,33 @@
+package net.protsenko;
+
+import lombok.Data;
+
+@Data
+public class Parameters {
+    private final static int ARGUMENTS_NUMBER = 2;
+    private String token;
+    private boolean sandBoxMode;
+
+    public Parameters(String token, boolean sandBoxMode) {
+        setParameter(token, sandBoxMode);
+    }
+
+    public Parameters(String[] args) {
+        if (args.length < 2) {
+            throw new IllegalArgumentException(String.format(
+                    "Invalid number of arguments [%d], excepted [%d]",
+                    args.length, ARGUMENTS_NUMBER));
+        }
+        setParameter(args[0], Boolean.parseBoolean(args[1]));
+    }
+
+    private void setParameter(String token, boolean sandBoxMode) {
+        this.token = token;
+        this.sandBoxMode = sandBoxMode;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("core.Parameters: sandBoxMode = %s", sandBoxMode ? "true" : "false");
+    }
+}
