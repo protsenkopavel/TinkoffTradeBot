8074d94b89f14a57721b9b13943dc0fcdc45eaf8
Pavel Protsenko <pavelprotsenko@outlook.com>
2024-01-19 01:15:53 +0300

    Add DB structure

---
+++ src/main/java/net/protsenko/models/entities/Candlestick.java
@@ -0,0 +1,49 @@
+package net.protsenko.models.entities;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+@Entity
+@Data
+public class Candlestick {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(value = AccessLevel.PRIVATE)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "timeframe_id")
+    private Timeframe timeframe;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "instrument_id")
+    private Instrument instrument;
+
+    private BigDecimal maximumValue;
+
+    private BigDecimal minimumValue;
+
+    private BigDecimal openedValue;
+
+    private BigDecimal closedValue;
+
+    private int volume;
+
+    private ZonedDateTime since;
+
+    @Override
+    public String toString() {
+        return String.format("%s [%s] %s: %.4f",
+                this.instrument.getTicker(),
+                this.timeframe.getCode(),
+                DateTimeTools.getTimeFormatted(this.since),
+                this.closedValue);
+    }
+
+}
